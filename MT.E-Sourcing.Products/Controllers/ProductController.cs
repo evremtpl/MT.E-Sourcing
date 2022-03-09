a370cb645f3cf09de2f849c93bfa4114681356c0
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-09 20:40:39 +0300

    Add project files.

---
+++ MT.E-Sourcing.Products/Controllers/ProductController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MT.E_Sourcing.Products.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+     
+    }
+}
