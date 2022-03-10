5bb644c0da663dacff5abb30393b01f1d9fd9dd2
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-10 22:11:55 +0300

    *[Product servisi docker da ayağa kaldırmak için dockerize işlemleri yapılmıştır.]

--- MT.E-Sourcing.Data/Settings/Interfaces/IProductDatabaseSettings.cs
+++ MT.E-Sourcing.Data/Settings/Interfaces/IProductDatabaseSettings.cs
@@ -4,7 +4,7 @@ namespace MT.E_Sourcing.Data.Settings.Interfaces
 {
    public interface IProductDatabaseSettings
     {
-        public string ConnectionStrings { get; set; }
+        public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
         public string CollectionName { get; set; }
 
