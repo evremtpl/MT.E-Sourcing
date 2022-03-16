0576271ad002dfe3adbb1cfe035e5be81578caf8
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-16 14:46:23 +0300

    *[OrderContext oluşturulmuştur.]

---
+++ MT.E-Sourcing.Order.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MT.E_Sourcing.Order.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        protected OrderContext(DbContextOptions<OrderContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Domain.Entities.Order> Orders { get; set; }
+    }
+}
