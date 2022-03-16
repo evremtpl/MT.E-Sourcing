e9d85b59b1e52ede3a06222f8ab8dd3f3b2c09a6
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-16 21:41:36 +0300

    *[Infrastructure katmanı Repository ve OrderRepository method implementasyonları.]

---
+++ MT.E-Sourcing.Order.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MT.E_Sourcing.Order.Domain.Repositories;
+using MT.E_Sourcing.Order.Infrastructure.Data;
+using MT.E_Sourcing.Order.Infrastructure.Repositories.Base;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MT.E_Sourcing.Order.Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Domain.Entities.Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext context) : base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Order>> GetOrdersBySellerUserName(string userName)
+        {
+            var orderList = await _context.Orders
+                .Where(o => o.SellerUserName == userName)
+                .ToListAsync();
+
+            return orderList;
+        }
+    }
+}
