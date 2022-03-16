c0c18bada3ba461ed5c931bfc47340372e77469d
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-16 14:32:39 +0300

    *[Doman katmanına repositories klasörü oluşturulmuştur.] *[Generic bir repository oluşturulup iç yapısı belirlenmiştir.] *[Order repository oluşturulmuştur.]

---
+++ MT.E-Sourcing.Order.Domain/Repositories/Base/IRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using MT.E_Sourcing.Order.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MT.E_Sourcing.Order.Domain.Repositories.Base
+{
+    public interface IRepository <T> where T: Entity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
+
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate=null,
+                                      Func<IQueryable<T>, IOrderedQueryable<T>> orderBy=null,
+                                      string includeString=null,
+                                      bool disableTracking=true);
+        Task<T> GetByIdAsync(int id);
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+    }
+}
