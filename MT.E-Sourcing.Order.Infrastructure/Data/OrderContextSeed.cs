659284e01d2bd665945f40d31923b8c1a018fcd6
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-16 15:18:56 +0300

    *[Migration işlemleri proje ilk ayağa kalktığında database oluşacak şekilde gerçekleştirilmiştir.] *[SeedData class oluşturulmuştur.]

---
+++ MT.E-Sourcing.Order.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,56 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MT.E_Sourcing.Order.Infrastructure.Data
+{
+   public static class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext)
+        {
+            if(!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreConfiguredOrders());
+                await orderContext.SaveChangesAsync();
+            }
+        }
+
+        private static IEnumerable<Domain.Entities.Order> GetPreConfiguredOrders()
+        {
+            return new List<Domain.Entities.Order>()
+           {
+               new Domain.Entities.Order()
+               {
+                   AuctionId=Guid.NewGuid().ToString(),
+                   ProductId = Guid.NewGuid().ToString(),
+                   SellerUserName="test@test.com",
+                   UnitPrice=10,
+                   TotalPrice=1000,
+                   CreateDate=DateTime.UtcNow
+               },
+                 new Domain.Entities.Order()
+               {
+                   AuctionId=Guid.NewGuid().ToString(),
+                   ProductId = Guid.NewGuid().ToString(),
+                   SellerUserName="test2@test.com",
+                   UnitPrice=10,
+                   TotalPrice=1000,
+                   CreateDate=DateTime.UtcNow
+               },
+                   new Domain.Entities.Order()
+               {
+                   AuctionId=Guid.NewGuid().ToString(),
+                   ProductId = Guid.NewGuid().ToString(),
+                   SellerUserName="test3@test.com",
+                   UnitPrice=10,
+                   TotalPrice=1000,
+                   CreateDate=DateTime.UtcNow
+               }
+
+           };
+        }
+    }
+}
---
+++ MT.E-Sourcing.Order/Extentions/MigrationManager.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MT.E_Sourcing.Order.Infrastructure.Data;
+using System;
+
+namespace MT.E_Sourcing.Order.Extentions
+{
+    public static class MigrationManager
+    {
+
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope= host.Services.CreateScope())
+            {
+                try
+                {
+                    var orderContext = scope.ServiceProvider.GetRequiredService<OrderContext>();
+                    if(orderContext.Database.ProviderName!= "Microsoft.EntityFrameworkCore.InMemory")
+                    {
+                        orderContext.Database.Migrate();
+                    }
+
+
+                    OrderContextSeed.SeedAsync(orderContext).Wait();
+                }
+                catch (Exception ex)
+                {
+
+                    throw ;
+                }
+            }
+
+            return host;
+        }
+    }
+}
