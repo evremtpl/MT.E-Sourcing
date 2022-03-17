4669e58937d34f9ba5d1a6dcc87a06e6ad45a3aa
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-17 10:27:19 +0300

    *[CQRS pattern Queries için geliştirmeler yapılmıştır.]

---
+++ MT.E-Sourcing.Order.Application/Queries/GetOrdersBySellerUserNameQuery.cs
@@ -0,0 +1,19 @@
+﻿
+
+using MediatR;
+using MT.E_Sourcing.Order.Application.Responses;
+using System.Collections.Generic;
+
+namespace MT.E_Sourcing.Order.Application.Queries
+{
+    public class GetOrdersBySellerUserNameQuery : IRequest<IEnumerable<OrderResponse>>
+    {
+
+        public string UserName { get; set; }
+
+        public GetOrdersBySellerUserNameQuery(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
