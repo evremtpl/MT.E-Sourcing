64c54776542f07e87e2f324cef8c1fcc75b55c44
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-17 09:53:41 +0300

    *[CQRS Pattern Application sınıfında uygulanmaya başlanmıştır.] *[Gerekli klasor yapısı kurulup, classlar oluşturulmuştur.]

---
+++ MT.E-Sourcing.Order.Application/Commands/OrderCreate/OrderCreateCommand.cs
@@ -0,0 +1,19 @@
+﻿
+
+using MediatR;
+using MT.E_Sourcing.Order.Application.Responses;
+using System;
+
+namespace MT.E_Sourcing.Order.Application.Commands.OrderCreate
+{
+    public class OrderCreateCommand : IRequest<OrderResponse>
+    {
+        
+        public string AuctionId { get; set; }
+        public string SellerUserName { get; set; }
+        public string ProductId { get; set; }
+        public decimal UnitePrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ MT.E-Sourcing.Order.Application/Commands/OrderCreate/OrderCreateValidator.cs
@@ -0,0 +1,15 @@
+﻿
+
+using FluentValidation;
+
+namespace MT.E_Sourcing.Order.Application.Commands.OrderCreate
+{
+    public class OrderCreateValidator :AbstractValidator<OrderCreateCommand>
+    {
+        public OrderCreateValidator()
+        {
+            RuleFor(v => v.SellerUserName).EmailAddress().NotEmpty();
+            RuleFor(v => v.ProductId).NotEmpty();
+        }
+    }
+}
