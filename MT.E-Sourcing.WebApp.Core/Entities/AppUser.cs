b4be6b8f59e7eda231d912944fe45b83b98a45bf
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-22 09:41:02 +0300

    *[Identity User -AppUser- oluşturuldu.] *[Infrastructure katmanında context sınıfı kodlandı.]

---
+++ MT.E-Sourcing.WebApp.Core/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace MT.E_Sourcing.WebApp.Core.Entities
+{
+   public class AppUser : IdentityUser
+    {
+        public string FirstName  { get; set; }
+        public string LasttName { get; set; }
+        public bool IsSeller { get; set; }
+        public bool IsBuyer { get; set; }
+    }
+}
---
+++ MT.E-Sourcing.WebApp.Infrastructure/Data/WebAppContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MT.E_Sourcing.WebApp.Core.Entities;
+
+namespace MT.E_Sourcing.WebApp.Infrastructure.Data
+{
+   public class WebAppContext : IdentityDbContext<AppUser>
+    {
+        public WebAppContext(DbContextOptions<WebAppContext> options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> AppUsers  { get; set; }
+    }
+}
